9258dce7025b7573e5dfdfe36ba9341f0c8c02c9
Chance Shaffer <shafferchance@gmail.com>
2019-04-21 18:39:01 -0400

    Finished project, the java part at-least. Had to change type of Id and do some more testing.

--- Main_Project/src/main/java/chance/Person.java
+++ Main_Project/src/main/java/chance/Person.java
@@ -2,14 +2,14 @@ package chance;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GeneraationType;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private String id;
 
     private String firstName;
     private String lastName;
