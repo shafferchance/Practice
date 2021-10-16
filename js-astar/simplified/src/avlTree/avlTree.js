de273712849df536fa4fe53b3d2d1691b893fa62
shafferchance <shafferchance@gmail.com>
2021-10-16 12:17:20 -0700

    Balancing factors working on mutation

--- js-astar/simplified/src/avlTree/avlTree.js
+++ js-astar/simplified/src/avlTree/avlTree.js
@@ -1,6 +1,6 @@
 class AVLNode {
     constructor(value, pointers) {
-        this.parent = points?.parent;
+        this.parent = pointers?.parent;
         this.left = pointers?.left;
         this.right = pointers?.right;
         this.value = value;
@@ -36,50 +36,45 @@ class AVL {
         return [0, current];
     }
 
-    calculateBalancingFactor(node, score) {
-        let [leftScore, rightScore] = [score, score];
-        if ((node ?? this).left) {
-            leftScore += 1;
-            return this.calculateBalancingFactor(
-                (node ?? this).left,
-                leftScore
-            );
-        }
+    calculateBalancingFactor(node) {
+        const leftScore =
+            (node.left?.balancingFactor ?? 0) + (node.left ? 1 : 0);
+        const rightScore =
+            (node.right?.balancingFactor ?? 0) + (node.right ? 1 : 0);
 
-        if ((node ?? this).right) {
-            rightScore += 1;
-            return this.calculateBalancingFactor(
-                (node ?? this).right,
-                rightScore
-            );
-        }
+        node.balancingFactor = leftScore - rightScore;
 
-        return score;
+        if (node.parent) {
+            return this.calculateBalancingFactor(node.parent);
+        }
     }
 
     insert(value) {
         const [direction, parent] = this._traversal(value);
 
         if (!parent) {
-            this.head = new BSTNode(value);
+            this.head = new AVLNode(value);
             return true;
         }
 
         switch (direction) {
             case -1: {
-                parent.left = new BSTNode(value, { parent });
+                parent.left = new AVLNode(value, { parent });
+                this.calculateBalancingFactor(parent.left);
                 break;
             }
             case 0: {
                 const temp = parent;
-                parent = new BSTNode(value, {
+                parent = new AVLNode(value, {
                     parent: temp.parent,
                     left: temp,
                 });
+                this.calculateBalancingFactor(parent);
                 break;
             }
             case 1: {
-                parent.right = new BSTNode(value, { parent });
+                parent.right = new AVLNode(value, { parent });
+                this.calculateBalancingFactor(parent.right);
                 break;
             }
             default: {
@@ -88,38 +83,41 @@ class AVL {
         }
     }
 
-    update(value, newValue, current) {
-        const [direction, parent] = this._traversal(value);
+    delete(value) {
+        const [direction, node] = this._traversal(value);
 
-        if (!parent) {
-            this.head = new BSTNode(value);
-            return true;
+        if (direction !== 0) {
+            throw new Error("Couldn't find node");
         }
 
-        switch (direction) {
-            case -1: {
-                parent.left = new BSTNode(value, { parent });
-                break;
+        const deleteNode = node;
+
+        // Since this is a BST, only the two cases should be possible
+        if (deleteNode.value < deleteNode.parent.value) {
+            // Re-Attaching detacted left tree
+            const leftNode = deleteNode.right ?? deleteNode.left;
+            if (leftNode !== deleteNode.left) {
+                leftNode.left = deleteNode.left;
             }
-            case 0: {
-                const temp = parent;
-                parent = new BSTNode(value, {
-                    parent: temp.parent,
-                    left: temp,
-                });
-                break;
+            deleteNode.parent.left = leftNode;
+            leftNode.parent = deleteNode.parent;
+            this.calculateBalancingFactor(leftNode);
+        } else {
+            // Re-Attaching detacted right tree
+            const rightNode = deleteNode.left ?? deleteNode.right;
+            console.log(
+                deleteNode.value,
+                deleteNode.left?.value,
+                deleteNode.right?.value
+            );
+            if (rightNode !== deleteNode.right) {
+                rightNode.right = deleteNode.right;
             }
-            case 1: {
-                parent.right = new BSTNode(value, { parent });
-                break;
-            }
-            default: {
-                throw new Error("Invalid direction recieved from traversal");
-            }
+            deleteNode.parent.right = rightNode;
+            rightNode.parent = deleteNode.parent;
+            this.calculateBalancingFactor(rightNode);
         }
     }
-
-    delete() {}
 }
 
 module.exports = {
--- js-astar/simplified/src/tests/avl.test.js
+++ js-astar/simplified/src/tests/avl.test.js
@@ -36,7 +36,67 @@ describe("CRUD AVL", () => {
         expect(avl.head.right.right.value).toBe(21);
     });
 
-    it("Update node: Depth 1", () => {});
+    it("Delete node: Depth 1", () => {
+        const avl = new AVL();
+        avl.insert(5);
+        avl.insert(10);
+        avl.insert(1);
 
-    it("Update node: Depth 2", () => {});
+        avl.delete(10);
+        expect(avl.head.value).toBe(5);
+    });
+
+    it("Delete node: Depth 2", () => {
+        const avl = new AVL();
+        avl.insert(5);
+        avl.insert(10);
+        avl.insert(3);
+        avl.insert(4);
+        avl.insert(1);
+        avl.insert(0);
+
+        avl.delete(3);
+        expect(avl.head.left.value).toBe(4);
+
+        avl.insert(8);
+        avl.insert(12);
+        avl.delete(10);
+        expect(avl.head.right.value).toBe(8);
+    });
 });
+
+describe("Balancing Factors within AVL", () => {
+    it("Complete and Full Tree", () => {
+        const avl = new AVL();
+        avl.insert(5);
+        avl.insert(1);
+        avl.insert(10);
+
+        expect(avl.head.balancingFactor).toBe(0);
+    });
+
+    // This test will break once turns are added, might have to mock insert function for this test to work properly
+    it("Unbalanced tree", () => {
+        const avl = new AVL();
+        avl.insert(10);
+        avl.insert(5);
+        avl.insert(1);
+
+        expect(avl.head.balancingFactor).toBe(2);
+    });
+
+    it("Rebalance after delete", () => {
+        const avl = new AVL();
+        avl.insert(5);
+        avl.insert(10);
+        avl.insert(3);
+        avl.insert(4);
+        avl.insert(1);
+        avl.insert(0);
+
+        avl.delete(3);
+        expect(avl.head.left.balancingFactor).toBe(2);
+    });
+});
+
+describe("Tree rotations", () => {});
